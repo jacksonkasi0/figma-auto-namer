b4cb582002e0cea81738f494b810ce6dc6510d77
Jackson Kasi <jacksonkasi@outlook.com>
2025-03-01 15:18:59 +0000

    undo

--- server/src/config/llm.ts
+++ server/src/config/llm.ts
@@ -2,7 +2,7 @@ import ModelClient, { isUnexpected } from "npm:@azure-rest/ai-inference";
 import { AzureKeyCredential } from "npm:@azure/core-auth";
 
 
-const token = Deno.env.get("GITHUB_TOKEN") || "xxxxxxxx";
+const token = Deno.env.get("GITHUB_TOKEN");
 const endpoint = Deno.env.get("AZURE_AI_ENDPOINT") || "https://models.inference.ai.azure.com";
 
 export const llm_client = ModelClient(
